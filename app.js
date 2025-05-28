786625c83cc234d26324ce70a8f67c1da68b567f
yuranicanon <yurani.canon@flyr.com>
2025-05-28 14:56:04 -0500

    suma y promedio del archivo data

--- app.js
+++ app.js
@@ -2,8 +2,19 @@ import { readFileSync, writeFileSync } from "fs";
 
 const data = readFileSync("./data.txt", "utf8").split("\n");
 
+let suma= 0;
+let promedio=0;
+
 for (let num of data) {
     console.log(num);
+    suma = suma + parseInt(num);
 }
 
-writeFileSync("./data.txt", data.join("\n"), "utf-8");
+promedio = suma / data.length; 
+
+console.log("La suma es " + suma);
+console.log("Promedio " + promedio);
+
+data.push(promedio);
+
+writeFileSync("./data.txt",  data.join("\n"), "utf-8");

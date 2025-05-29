1ecb2fdcf090d0ecefb239832e7e1f713db7749b
yuranicanon <yurani.canon@flyr.com>
2025-05-29 14:40:53 -0500

    Cambio en la data y en la última posición

--- app.js
+++ app.js
@@ -15,6 +15,13 @@ promedio = suma / data.length;
 console.log("La suma es " + suma);
 console.log("Promedio " + Math.round(promedio));
 
-data.push(promedio);
+if(data.length <= 1000)
+{
+    data.push(Math.round(promedio));
+}
+else{
+    data[data.length-1] = Math.round(promedio);
+}
+
 
 writeFileSync("./data.txt",  data.join("\n"), "utf-8");
